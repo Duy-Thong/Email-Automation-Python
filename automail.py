60040f37f11071f12310fabe777d869ca1a4670c
Dao Duy Thong <duythong.ptit@gmail.com>
2023-09-06 23:06:21 +0700

    Update automail.py

--- automail.py
+++ automail.py
@@ -2,54 +2,54 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import openpyxl
-#Đường dẫn đến các file 
-data_path=r'D:\Work_space\Email-Automation-Python\Infor.xlsx'   #thay đường dẫn bằng đường dẫn đến file xlxs
-index_path=r'D:\Work_space\Email-Automation-Python\index.html'
 
-# Thông tin về tài khoản email của bạn
-sender_email = "clbsomediaptit@gmail.com"
-sender_password = "gvszzolbdxqlviyi"
+# Paths to the files
+data_path = r'D:\Work_space\Email-Automation-Python\Infor.xlsx'   # Replace with the path to your Excel file
+index_path = r'D:\Work_space\Email-Automation-Python\index.html'
 
-# Đọc nội dung email từ tệp HTML
+# Your email account information
+sender_email = "YOUR_EMAIL_ADDRESS"
+sender_password = "YOUR_EMAIL_PASSWORD"
+
+# Read email content from the HTML file
 with open(index_path, "r", encoding="utf-8") as html_file:
     email_content = html_file.read()
 
-
-# Đọc dữ liệu từ file Excel
+# Read data from the Excel file
 wb = openpyxl.load_workbook(data_path)
 sheet = wb.active
 
-# Lấy danh sách thông tin người nhận từ cột A (Full name) và cột B (Email)
+# Get a list of recipient information from column A (Full name) and column B (Email)
 recipients = []
 for row in sheet.iter_rows(min_row=2, values_only=True):
-    full_name, email, *_ = row  # Chỉ quan tâm đến cột A và cột B
+    full_name, email, *_ = row  # Only interested in columns A and B
     if email:
         recipients.append((full_name, email))
-        
-# Kết nối đến máy chủ SMTP của Gmail
+
+# Connect to the Gmail SMTP server
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 server = smtplib.SMTP(smtp_server, smtp_port)
 server.starttls()
 
-# Đăng nhập vào tài khoản email
+# Log in to the email account
 server.login(sender_email, sender_password)
 
-# Gửi email cho từng người nhận
+# Send emails to each recipient
 for full_name, receiver_email in recipients:
-    # Tạo đối tượng MIMEMultipart để tạo email
+    # Create a MIMEMultipart object to compose the email
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
-    message["Subject"] = "[ THÔNG BÁO KẾT QUẢ VÒNG CV - SỔ MEDIA ]"
+    message["Subject"] = "[ NOTIFICATION OF ROUND CV RESULTS - MEDIA BOOK ]"
     email_content_with_name = email_content.replace("$NAME", full_name)
-    # Nội dung email lấy từ tệp HTML
+    # Email content is taken from the HTML file
     message.attach(MIMEText(email_content_with_name, "html"))
 
-    # Gửi email
+    # Send the email
     server.sendmail(sender_email, receiver_email, message.as_string())
 
-# Đóng kết nối SMTP
+# Close the SMTP connection
 server.quit()
 
-print("Email đã được gửi thành công!")
+print("Emails have been sent successfully!")
